7d29d62c9e4a9bc0cbe1b58f83a1f05d95df9138
Allegra Saggese <111875712+allegra-saggese-econ@users.noreply.github.com>
2023-03-07 12:23:08 +0000

    re-upload location data
    - reupload recycling drop off points with data manually adjusted to have lat/long
    - could not use geocode package as it requires money to pull POS from Google API

--- battery_data_clean.R
+++ battery_data_clean.R
@@ -7,19 +7,25 @@ library(ggplot2)
 # include packages for geocoding and location service
 library(ggmap)
 library(mapview)
-
+# include packages for handling spatial data
 
 
 # upload store data
-drop_df <- read_csv("battery_recycling_points.csv", 
+drop_df <- read_csv("battery_recycling_points_full.csv", 
                       col_types = cols(Lat = col_number(), 
                       Long = col_number(), `Confirmed via call` = col_factor(levels = c("Y", 
                       "N")), `Battery drop off present?` = col_factor(levels = c("Y", "N")))) # upload data - make Y/N column a factor
 
 # geocode addresses for map use
+## cannot geocode beause I need to set up billing - given the low number of 
+## addresses, I'm just going to manually check for lat/long
+#re-upload data with geocoded lat/long
 
 
 # upload geo-data including borough, ward. LSOA boundary maps
+
+
+
 # upload bus and bike route data for overlay (step 2)
 
 # merge and make map 
